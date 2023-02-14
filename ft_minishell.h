de4e8d5ed0a71a2cf88a87e4169bc103dc319621
GaelGosse <gael.gosse@yahoo.fr>
2023-02-14 11:31:01 +0100

    gael:header_file

---
+++ ft_minishell.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_minishell.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gael <gael@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/14 11:14:58 by gael              #+#    #+#             */
+/*   Updated: 2023/02/14 11:18:34 by gael             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_MINISHELL_H
+# define FT_MINISHELL_H
+
+# include <dirent.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <limits.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/time.h>
+# include <sys/resource.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <sys/ioctl.h>
+# include <signal.h>
+# include <stdarg.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <string.h>
+# include <termios.h>
+# include <unistd.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+# define SUCCESS 1
+# define FAIL -1
+
+
+
+#endif
